5eeb6181a2efae8f065832116d8b7b839f181313
Ryan Jasinski <rjasinski@indeed.com>
2021-05-08 11:05:29 -0400

    task 3 completed

--- task3.js
+++ task3.js
@@ -1,2 +1,14 @@
 // Task 3: using https://restcountries.eu/ API,
 // get country where alpha3Code = col
+
+const fetchData = async () => {
+  //grab the data from the API, wait for response before moving on
+  const res = await fetch("https://restcountries.eu/rest/v2/alpha/col");
+
+  //consver to JSON so we can manipulate it if needed
+  const country = await res.json();
+
+  console.log(country);
+};
+
+fetchData();
